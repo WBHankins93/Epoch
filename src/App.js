a98c81be8167c98c6a1834ad47125e1b8692b508
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-27 14:40:28 -0600

    added title video to landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+
+        <img class="image" src="https://media.giphy.com/media/874Z2F7ZmX1GWbRaWw/giphy.gif" />
+
       </div>
     );
   }
