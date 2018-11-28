6409830676f4a24b964ea370123323f24bdf2186
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-28 14:14:54 -0600

    added route to main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import LandingPage from './LandingPage';
+import HomePage from './HomePage';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-        <LandingPage />
+
+
+        <Switch>
+          <Route exact path="/" component={LandingPage}/>
+          <Route exact path="/home" component={HomePage}/>
+
+        </Switch>
       </div>
     );
   }
