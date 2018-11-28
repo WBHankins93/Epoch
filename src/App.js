2e4b7f563ea86aecd6ef2f9e036745a585e9472d
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-28 13:22:04 -0600

    Landing Page Component created. App is live, LandingPage.css file added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import LandingPage from './LandingPage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
 
-        <img class="image" src="https://media.giphy.com/media/874Z2F7ZmX1GWbRaWw/giphy.gif" />
-
+        <LandingPage />
       </div>
     );
   }
---
+++ src/LandingPage/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './LandingPage.css';
+import { Button } from 'semantic-ui-react';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <div>
+      <img class="image" alt="" src="https://media.giphy.com/media/874Z2F7ZmX1GWbRaWw/giphy.gif" />
+      </div>
+        <p>"A period of time in history or a person's life, typically one marked by notable events or particular characteristics."</p>
+
+
+
+        <br/>
+        <br/>
+          <div>
+            <Button class='ui primary button' role='button'>
+              Primary
+            </Button>
+            <Button class='ui secondary button' role='button'>
+              Secondary
+            </Button>
+          </div>
+          <div>
+          <Button size='massive'>Massive</Button>
+          </div>
+
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
